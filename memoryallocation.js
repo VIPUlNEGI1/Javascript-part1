806b8bd40636c68da9d85d8b72e559ec8b6bd839
VIPUlNEGI1 <167672450+VIPUlNEGI1@users.noreply.github.com>
2024-08-16 03:00:22 +0000

    new

---
+++ memoryallocation.js
@@ -0,0 +1,15 @@
+// therre are two types of memory 
+// stack=primitive heap=nonprimitive
+
+let myName = "vipulnegi"
+let secoundname = kirtik
+
+secoundname = "rohan"
+console.log(secoundname)
+console.log(myName)
+
+let user = {
+    email : "99vhipul*22@gmail.com",
+    upi : "fgjldgf@ybl"
+}
+console.log(user.email)
