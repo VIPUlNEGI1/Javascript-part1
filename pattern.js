9398c5ccad65df246303b0c039023353d6f4c313
vipul negi <167672450+VIPUlNEGI1@users.noreply.github.com>
2026-05-28 07:31:46 +0000

    feat ::dsfadf

---
+++ pattern.js
@@ -0,0 +1,44 @@
+// let n = 5
+// for(let i = 1; i <= n; i++){
+// let row = "";
+
+// for (let p = 1 ; p <= i ;p++){
+// row += "*";
+// }
+// console.log(row)
+// }
+
+// let a=5
+// for (let i = 1 ; i >= a ; i--){
+//     let row = "";
+//     for (let p = 1; p<=i; i++){
+//         row += "+"
+//     }
+//     console.log(row)
+// }
+
+// let a = 5;
+
+// for (let i = a; i >= 1; i--) {
+
+//     let row = "";
+
+//     for (let p = 1; p <= i; p++) {
+//         row += "+";
+//     }
+
+//     console.log(row);
+// }
+
+// +++
+// ++
+// +
+
+const a = 10 
+for(let i = 1 ; i<= a; i++){
+    let row = "";
+    for (p=10 ; p>=1; p--){
+      row +="*"
+    }
+    console.log(row)
+}
